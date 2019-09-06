185443ad5b96007b6d5e507871894907ed423f73
wenha <ywh_wh@126.com>
2019-09-06 18:13:55 +0800

    添加窗体

--- CodeMaker/Form_TemplateTree.cs
+++ CodeMaker/Form_TemplateTree.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace CodeMaker
 {
-    public partial class Form1 : Form
+    public partial class Form_TemplateTree : Form
     {
-        public Form1()
+        public Form_TemplateTree()
         {
             InitializeComponent();
         }
